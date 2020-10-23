fbb848d1aa285fe8c3e1f13588fa086fba9993ed
Nikolina Josić <njosic@pmfst.hr>
2020-10-23 13:44:55 +0200

    backend

---
+++ build copy/precache-manifest.74bd33ad9b42a65b40b2eca35854b5b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33cd5d6404cab33dc8da4ba34768a014",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef5f33857c47f2196774",
+    "url": "/static/css/main.90ffd6ca.chunk.css"
+  },
+  {
+    "revision": "75e5ba4b459a4b80655a",
+    "url": "/static/js/2.7d5328f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7d5328f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef5f33857c47f2196774",
+    "url": "/static/js/main.31151028.chunk.js"
+  },
+  {
+    "revision": "82664534ad3b166a4ff3",
+    "url": "/static/js/runtime-main.5763738a.js"
+  }
+]);
